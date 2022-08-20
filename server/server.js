ada71804eb59604702065c161125a59cd89c53e4
Teddy Liang <teddytduoliang@gmail.com>
2022-08-20 11:52:40 -0700

    created mongo database and connected to it

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 // express app
 const app = express();
@@ -15,7 +16,14 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/workouts', workoutRoutes)
 
-//listen for reqeusts
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for reqeusts
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db and listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
