68cb222c7cc7efe178b83625068beaa2ddfb57b9
Teddy Liang <teddytduoliang@gmail.com>
2022-08-20 17:15:42 -0700

    added fetching and displaying data capabilities

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import{BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-// pages and components
+// pages & components
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 
 function App() {
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <div className="pages">
           <Routes>
-            <Route
-              path="/"
-              element={<Home />}
+            <Route 
+              path="/" 
+              element={<Home />} 
             />
           </Routes>
         </div>
       </BrowserRouter>
-
     </div>
   );
 }
